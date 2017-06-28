79a6fa3e68cf5c115df7d86c07c33623fca7d9f1
kpack <kevinpackler@gmail.com>
2017-06-27 22:49:07 -0400

    change

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,24 +1,17 @@
-
-var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chincilla", "hedgehog", "hermit crab", "gerbil", "pygmy" "goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"]
-
-function renderButtons() {
-far (var i = 0; i < animals.length; i++){
-var newButton = $("<button>");
-$("#animalButtons").append(newButton);
-newButton.text
+$(document).ready(function() {
+	var animals = ["dog", "cat", "rabbit"];
+	function renderButtons() {
+		for (var i = 0; i < animals.length; i++){
+		var newButton = $('<button>');
+		$('#animalButtons').append(newButton);
+		newButton.text(animals[i])
+		}
 }
 
-  $("#addAnimal").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
+$('#addAnimal').on('click', function(event) {
         event.preventDefault();
-
-        // This line will grab the text from the input box
-        var newAnimal = $("#addAnimal").val().trim();
-        // The movie from the textbox is then added to our array
+        var newAnimal = $('#addAnimal').val().trim();
         newAnimal.push(animals);
-
-        // calling renderButtons which handles the processing of our movie array
         renderButtons();
       });
 
@@ -37,7 +30,7 @@ $(this).attr("data-state", "animate");
 } else {
   $(this).attr("src", $(this).attr("data-still"));
   $(this).attr("data-state", "still");
-}
+};
 
 
 
